891f0b4a40b78dc65f4145ea5b1313cd36fd3daf
Mo7ammadSale7 <mo7mmad57@gamil.com>
2021-06-18 15:20:18 +0200

    Add User Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 from .models import Meal, Rating
+
+
+User = get_user_model()
 
 
 class MealSerializer(serializers.ModelSerializer):
@@ -13,3 +18,15 @@ class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = ('id', 'stars', 'user', 'meal')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', )
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            token = Token.objects.create(user=user)
+            return token
