fc6e6b6f3b5794e2e2459827946070bee2fca38e
Mo7ammadSale7 <mo7mmad57@gamil.com>
2021-06-18 16:33:03 +0200

    Clean Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
-
 from .models import Meal, Rating
 
 
@@ -25,8 +23,3 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'password', )
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
-
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            token = Token.objects.create(user=user)
-            return token
